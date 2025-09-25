5eea345dc5a7c480ea5194ea4db5ce4e6f2208bd
jac7az <jac7az@gmail.com>
2025-09-25 06:36:52 +0000

    clean.py and load.py edits

--- clean.py
+++ clean.py
@@ -1,2 +1,85 @@
 import duckdb
 import logging
+
+logging.basicConfig(
+    level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s',
+    filename='load.log'
+)
+logger = logging.getLogger(__name__)
+
+def clean_parquet_files():
+
+    con = duckdb.connect(database='emissions.duckdb', read_only=False)
+    logger.info("Connected to emissions.duckdb for cleaning")
+
+    #Eliminate duplicate entries and entries with null variables
+    try:  
+        con.execute(f"""CREATE TABLE clean_yellow_tripdata AS SELECT DISTINCT * FROM yellow_tripdata;
+                    "DROP TABLE yellow_tripdata;
+                    ALTER TABLE clean_yellow_tripdata RENAME TO yellow_tripdata;""")
+        con.execute(f"""DELETE FROM yellow_tripdata WHERE VendorID IS NULL
+                    OR tpep_pickup_datetime IS NULL
+                    OR tpep_dropoff_datetime IS NULL
+                    OR tpep_dropoff_datetime - tpep_pickup_datetime > INTERVAL '24 hours'
+                    OR passenger_count IS NULL
+                    OR passenger_count=0
+                    OR trip_distance IS NULL
+                    OR trip_distance>100;
+                    """)
+        logging.info("Created clean version of yellow tripdata")
+
+        con.execute(f"""CREATE TABLE clean_green_tripdata AS SELECT DISTINCT * FROM green_tripdata;
+                    DROP TABLE green_tripdata;
+                    ALTER TABLE clean_green_tripdata RENAME TO green_tripdata;""")
+        con.execute(f"""DELETE FROM green_tripdata WHERE VendorID IS NULL
+                    OR lpep_pickup_datetime IS NULL
+                    OR lpep_dropoff_datetime IS NULL
+                    OR lpep_dropoff_datetime - lpep_pickup_datetime > INTERVAL '24 hours'
+                    OR passenger_count IS NULL
+                    OR passenger_count=0
+                    OR trip_distance IS NULL
+                    OR trip_distance>100;
+                    """)
+        logging.info("Created clean version of green tripdata")
+
+        con.execute(f"""CREATE TABLE clean_emissions AS SELECT DISTINCT * FROM emissions;
+                    DROP TABLE emissions;
+                    ALTER TABLE clean_emissions RENAME TO emissions;""")
+        logging.info("Create clean table for emissions")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        logger.error(f"An error occurred: {e}")
+
+def clean_test():
+    con = duckdb.connect(database='emissions.duckdb', read_only=False)
+    logger.info("Connected to emissions.duckdb for clean testing")
+    try:
+        #test to see if there are any remaining entries that break the conditions in yellow_tripdata
+        print(con.execute(f"""SELECT COUNT(*) FROM yellow_tripdata WHERE VendorID IS NULL
+                                 OR tpep_pickup_datetime IS NULL OR tpep_dropoff_datetime IS NULL 
+                                 OR tpep_dropoff_datetime - tpep_pickup_datetime > INTERVAL '24 hours'
+                                 OR passenger_count IS NULL OR passenger_count=0
+                                 OR trip_distance IS NULL OR trip_distance>100;""").fetchone()[0])
+        if con.execute("SELECT COUNT(*) FROM yellow_tripdata;").fetchone()[0]-con.execute("SELECT COUNT(DISTINCT *) FROM yellow_tripdata;").fetchone()[0]==0:
+            print("No duplicates found.")
+            logging.info("No duplicate or incorrect entries in yellow_tripdata")
+        else:
+            logging.error("Duplicates found")
+        
+        #test to see if there are any remaining entries that break the conditions in green_tripdata   
+        print(con.execute(f"""SELECT COUNT(*) FROM green_tripdata WHERE VendorID IS NULL
+                                 OR lpep_pickup_datetime IS NULL OR lpep_dropoff_datetime IS NULL 
+                                 OR lpep_dropoff_datetime - lpep_pickup_datetime > INTERVAL '24 hours'
+                                 OR passenger_count IS NULL OR passenger_count=0
+                                 OR trip_distance IS NULL OR trip_distance>100;""").fetchone()[0]) 
+        if con.execute("SELECT COUNT(*) FROM green_tripdata;").fetchone()[0]-con.execute("SELECT COUNT(DISTINCT *) FROM green_tripdata;").fetchone()[0]==0:
+            print("No duplicates found.")
+            logging.info("No duplicate or incorrect entries in green_tripdata")
+        else:
+            logging.error("Duplicates found")
+    except Exception as e:
+        print(f"Clean testing error has occured:{e}")
+
+if __name__ == "__main__":
+    clean_parquet_files()
+    clean_test()
--- load.py
+++ load.py
@@ -1,6 +1,7 @@
 import duckdb
 import os
 import logging
+import pandas as pd
 
 logging.basicConfig(
     level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s',
@@ -16,11 +17,39 @@ def load_parquet_files():
         # Connect to local DuckDB instance
         con = duckdb.connect(database='emissions.duckdb', read_only=False)
         logger.info("Connected to DuckDB instance")
+        
+        #Initialize the table using Jan2014 data. Will remove duplicates in cleaning.
+        con.execute(f"""CREATE OR REPLACE TABLE IF NOT EXISTS yellow_tripdata AS SELECT * FROM read_parquet('https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2014-01.parquet');""")
+        con.execute(f"""CREATE OR REPLACE TABLE IF NOT EXISTS green_tripdata AS SELECT * FROM read_parquet('https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_2014-01.parquet');""")
+        logger.info("Created green and yellow tables")
+         #Get a list of all 10-years worth of files for yellow taxi trips and green taxi trips and put them into lists so sql insert into table.
+        for year in range(2014,2025):
+            for month in range(1,13):
+                con.execute(f"""INSERT INTO yellow_tripdata SELECT * FROM read_parquet('https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year}-{month:02}.parquet');
+                            INSERT INTO green_tripdata SELECT * FROM read_parquet('https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_{year}-{month:02}.parquet');""")
+        logger.info("Successfully added 10 years of files into each table")
 
-        con.execute(f"""
-            -- SQL goes here
-        """)
-        logger.info("Dropped table if exists")
+        #Create separate table for emissions
+        con.execute(f"""CREATE TABLE IF NOT EXISTS emissions AS SELECT * FROM read_csv('data/vehicle_emissions.csv')""")
+        logger.info("created emissions table")
+
+        #Output basic descriptive stats to screen and to log.
+        columns=['column_name','column_type','null','key','default','extra']
+        yellow_stats=con.execute(f"""DESCRIBE SELECT * FROM yellow_tripdata;""")
+        yellow_data = pd.DataFrame(yellow_stats.fetchall(),columns=columns)
+        logging.info(yellow_data['column_name'])
+
+        green_stats=con.execute(f"""DESCRIBE SELECT * FROM green_tripdata;""") 
+        green_data=pd.DataFrame(green_stats.fetchall(),columns=columns)
+        logging.info(green_data['column_name'])
+        
+        emission_stats=con.execute(f"""DESCRIBE SELECT * FROM emissions;""") 
+        emission_data=pd.DataFrame(emission_stats.fetchall(),columns=columns)
+        logging.info(emission_data['column_name'])
+        
+        print(yellow_data,'\n')
+        print(green_data,'\n')
+        print(emission_data)
 
     except Exception as e:
         print(f"An error occurred: {e}")
