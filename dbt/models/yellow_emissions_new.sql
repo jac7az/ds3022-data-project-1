f5347a9df8a23233af4c1dfc7db259b7c67e0044
jac7az <jac7az@gmail.com>
2025-09-26 06:07:29 +0000

    transformation edits

---
+++ dbt/models/yellow_emissions_new.sql
@@ -0,0 +1,17 @@
+SELECT 
+    y.tpep_pickup_datetime, 
+    y.tpep_dropoff_datetime,
+    y.passenger_count,
+    y.trip_distance,
+    (y.trip_distance * e.co2_grams_per_mile) / 1000.0 AS trip_co2_kgs,
+
+    CASE 
+        WHEN date_diff('second',y.tpep_pickup_datetime,y.tpep_dropoff_datetime)>0 
+        THEN y.trip_distance/(date_diff('second',y.tpep_pickup_datetime,y.tpep_dropoff_datetime)/3600)
+        ELSE 0.000 
+        END AS avg_mph,
+    HOUR(y.tpep_pickup_datetime) AS hour_of_day,
+    DAYNAME(y.tpep_pickup_datetime) AS day_of_week,
+    WEEKOFYEAR(y.tpep_pickup_datetime) AS week_of_year,
+    MONTHNAME(y.tpep_pickup_datetime) AS month_of_year
+FROM {{ source('nyc_taxi_data', 'yellow_tripdata') }} AS y
--- load.py
+++ load.py
@@ -2,6 +2,7 @@ import duckdb
 import os
 import logging
 import pandas as pd
+import time
 
 logging.basicConfig(
     level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s',
@@ -18,15 +19,17 @@ def load_parquet_files():
         con = duckdb.connect(database='emissions.duckdb', read_only=False)
         logger.info("Connected to DuckDB instance")
         
-        #Initialize the table using Jan2014 data. Will remove duplicates in cleaning.
+        #Initialize the table 
         con.execute("""CREATE OR REPLACE TABLE yellow_tripdata (tpep_pickup_datetime TIMESTAMP, tpep_dropoff_datetime TIMESTAMP,passenger_count BIGINT, trip_distance DOUBLE);""")
         con.execute("""CREATE OR REPLACE TABLE green_tripdata (lpep_pickup_datetime TIMESTAMP, lpep_dropoff_datetime TIMESTAMP,passenger_count BIGINT, trip_distance DOUBLE);""")
         logger.info("Created green and yellow tables")
+
          #Get a list of all 10-years worth of files for yellow taxi trips and green taxi trips and put them into lists so sql insert into table.
         for year in range(2014,2025):
             for month in range(1,13):
                 con.execute(f"""INSERT INTO yellow_tripdata SELECT tpep_pickup_datetime, tpep_dropoff_datetime,passenger_count, trip_distance FROM read_parquet('https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{year}-{month:02}.parquet');
                             INSERT INTO green_tripdata SELECT lpep_pickup_datetime, lpep_dropoff_datetime,passenger_count, trip_distance FROM read_parquet('https://d37ci6vzurychx.cloudfront.net/trip-data/green_tripdata_{year}-{month:02}.parquet');""")
+                time.sleep(30)
         logger.info("Successfully added 10 years of files into each table")
 
         #Create separate table for emissions
